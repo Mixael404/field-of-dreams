0336aac2cf3be326b3bd82d47991be5987b209ff
Mikhail Slutskii <slutskij.m.a@gmail.com>
2023-11-03 18:10:39 +0300

    Refactoring of functions letterInput and wordInput

--- js/script.js
+++ js/script.js
@@ -11,22 +11,24 @@ class Game {
         const isCookiesHaveWord = isCookiesHaveKey("word");
         const isCookiesHaveCurrentPlayer = isCookiesHaveKey("currentplayer");
         console.log(isCookiesHaveState);
-        if(!isCookiesHaveState){
+
+        if (!isCookiesHaveState) {
             this.createCookieKey("state=[]");
         }
-        if(!isCookiesHavePlayers){
+        if (!isCookiesHavePlayers) {
             this.createCookieKey("players=[]");
         }
-        if(!isCookiesHaveWord){
+        if (!isCookiesHaveWord) {
             this.createCookieKey("word=[]");
         }
-        if (!isCookiesHaveCurrentPlayer){
+        if (!isCookiesHaveCurrentPlayer) {
             this.createCookieKey("currentplayer=")
         }
 
 
         this.root = document.querySelector(setup.root);
-        if(!this.root.classList.contains("relative")){
+
+        if (!this.root.classList.contains("relative")) {
             this.root.classList.add("relative");
         }
 
@@ -44,13 +46,14 @@ class Game {
         this.newGameBtn = this.root.querySelector(".newGame");
 
         this.amountOfPlayers = setup.players;
-        this.wordsForPlay = setup.words;        
+        this.wordsForPlay = setup.words;
         this.fullScore = new Array(setup.players).fill(0);
         this.players = new Array(this.amountOfPlayers);
         this.currentPlayer = 0;
 
         this.init();
         this.scoreRow = this.table.children[1];
+
         this.addEventListeners();
         this.playerLabel.textContent = `Ход игрока ${(this.currentPlayer + 1)}`;
     }
@@ -69,7 +72,7 @@ class Game {
         this.playerLabel.textContent = `Ход игрока ${(this.currentPlayer + 1)}`;
     }
 
-    addEventListeners(){
+    addEventListeners() {
         this.fullWordBtn.addEventListener("click", this.changeInputType.bind(this));
         this.oneLetterBtn.addEventListener("click", this.changeInputType.bind(this));
         this.checkBtn.addEventListener("click", this.lettersInput.bind(this));
@@ -79,12 +82,12 @@ class Game {
 
 
 
-    createElement(tag, wrapper, styleClass, text){
+    createElement(tag, wrapper, styleClass, text) {
         const element = document.createElement(tag);
-        if(element.tagName == "INPUT"){
+        if (element.tagName == "INPUT") {
             element.maxLength = "1";
         }
-        if(text){
+        if (text) {
             element.textContent = text;
         }
         element.classList.add(styleClass);
@@ -100,12 +103,12 @@ class Game {
         }
     }
 
-    setScoreTable(){
+    setScoreTable() {
         this.table.children[0].innerHTML = "";
         this.table.children[1].innerHTML = "";
-        for (let i = 0; i < this.players.length; i++){
-            const playerName = document.createElement("td","", "");
-            const playerScore = document.createElement("td", "" , "")
+        for (let i = 0; i < this.players.length; i++) {
+            const playerName = document.createElement("td", "", "");
+            const playerScore = document.createElement("td", "", "")
             playerName.textContent = "player " + (i + 1);
             playerScore.textContent = this.players[i];
             this.table.children[0].append(playerName);
@@ -113,8 +116,8 @@ class Game {
         }
     }
 
-    updateScoreTable(scoreArr){
-        for (let i = 0; i < this.players.length; i++){
+    updateScoreTable(scoreArr) {
+        for (let i = 0; i < this.players.length; i++) {
             this.scoreRow.children[i].textContent = scoreArr[i];
         }
     }
@@ -130,8 +133,11 @@ class Game {
     }
 
     drawLetters() {
-        this.state.forEach((letter, index) =>{
-            if (letter != undefined){
+        this.state.forEach((letter, index) => {
+            // false , null, NaN,
+            // 0, "0"
+            // if (letter){
+            if (letter != undefined) {
                 this.wordWrapper.children[index].textContent = letter;
                 this.fieldsWrapper.children[index].value = letter;
                 this.fieldsWrapper.children[index].disabled = true;
@@ -140,7 +146,7 @@ class Game {
     }
 
 
-    changeInputType() {
+    changeInputType(typeOperation) {
         this.wordFullSection.classList.toggle("collapsed");
         this.letterSection.classList.toggle("collapsed");
         this.fullWordBtn.classList.toggle("collapsed");
@@ -148,7 +154,10 @@ class Game {
     }
 
     lettersInput() {
-        if (this.letterSection.classList.contains("collapsed")) {
+        const letterInputHidden = this.letterSection.classList.contains("collapsed");
+        const wordIncludesLetter = this.wordArray.includes(this.inputLetter.value);
+        const letterAlreadyWas = this.checkedLetters.includes(this.inputLetter.value);
+        if (letterInputHidden) {
             return;
         }
 
@@ -156,18 +165,18 @@ class Game {
             return;
         }
 
-        if (this.checkedLetters.includes(this.inputLetter.value)){
+        if (letterAlreadyWas) {
             this.inputLetter.value = "";
             return;
         }
-        if (!this.wordArray.includes(this.inputLetter.value)) {
+        if (!wordIncludesLetter) {
             this.inputLetter.value = "";
             this.nextPlayer();
         }
-        if (this.wordArray.includes(this.inputLetter.value)) {
+        if (wordIncludesLetter) {
             const letter = this.inputLetter.value;
             const indexes = this.findLetters(letter);
-            for (let index of indexes){
+            for (let index of indexes) {
                 this.state[index] = letter;
             }
             this.drawLetters();
@@ -178,8 +187,6 @@ class Game {
             this.pushToCookie("players", this.players)
             this.pushToCookie("state", this.state)
         }
-        console.log(this.state);
-        console.log(this.players);
         this.checkWinByLastLetterInput();
         this.playerLabel.textContent = `Ход игрока ${(this.currentPlayer + 1)}`;
     }
@@ -187,13 +194,17 @@ class Game {
 
 
     wordWin() {
-        if (this.wordFullSection.classList.contains("collapsed")) {
+        const wordFullInputHidden = this.wordFullSection.classList.contains("collapsed");
+        if (wordFullInputHidden) {
             return;
         }
         let word = [];
+
         for (let i = 0; i < this.fieldsWrapper.children.length; i++) {
             word.push(this.fieldsWrapper.children[i].value);
         }
+
+
         if (word.join("") == this.wordArray.join("")) {
             console.log("Победил " + (this.currentPlayer + 1));
             this.players[this.currentPlayer] += 1000;
@@ -202,7 +213,7 @@ class Game {
         } else {
             // Стирание активных инпутов после неправильного ввода слова
             for (let i = 0; i < this.fieldsWrapper.children.length; i++) {
-                if(this.fieldsWrapper.children[i].disabled == false){
+                if (this.fieldsWrapper.children[i].disabled == false) {
                     this.fieldsWrapper.children[i].value = "";
                 }
             }
@@ -214,8 +225,8 @@ class Game {
         }
     }
 
-    refreshAfterWin(){
-        for (let i = 0; i < this.fullScore.length; i++){
+    refreshAfterWin() {
+        for (let i = 0; i < this.fullScore.length; i++) {
             this.fullScore[i] += this.players[i];
         }
         console.log(this.fullScore);
@@ -226,8 +237,8 @@ class Game {
         this.startNewGame();
     }
 
-    checkWinByLastLetterInput(){
-        if (this.state.toString() == this.wordArray.toString()){
+    checkWinByLastLetterInput() {
+        if (this.state.toString() == this.wordArray.toString()) {
             console.log("Победа!");
             this.players[this.currentPlayer] += 300;
             this.updateScoreTable(this.players);
@@ -235,19 +246,19 @@ class Game {
         }
     }
 
-    showWin(){
+    showWin() {
         this.createElement("p", this.root, "winString", `Победил ${this.currentPlayer + 1}`);
         setTimeout(this.deleteWin, 2000);
     }
 
     //TODO находить строку в root вместо document
-    deleteWin(){
+    deleteWin() {
         const str = document.querySelector(".winString");
         str.remove();
     }
 
-    nextPlayer(){
-        if (this.currentPlayer == this.players.length - 1){
+    nextPlayer() {
+        if (this.currentPlayer == this.players.length - 1) {
             this.currentPlayer = 0;
         } else {
             this.currentPlayer++;
@@ -255,31 +266,31 @@ class Game {
         document.cookie = "currentplayer=" + this.currentPlayer;
     }
 
-    generateRandomInt(max){
+    generateRandomInt(max) {
         let maxNumber = Math.floor(Math.random() * max);
-        if (maxNumber == 0){
+        if (maxNumber == 0) {
             maxNumber = 1;
         }
         return maxNumber;
     }
 
-    generateWord(){
+    generateWord() {
         this.word = this.wordsForPlay[this.generateRandomInt(6)];
         console.log(this.word);
         this.lowerCaseWord = this.word.toLowerCase();
         this.encodedWord = encodeURIComponent(this.lowerCaseWord);
         this.wordArray = this.lowerCaseWord.split("");
+        //  pushToCookie(key, data)
         document.cookie = "word=" + this.encodedWord;
     }
 
-    startNewGame(){
+    startNewGame() {
         document.cookie = "state=[]";
         this.players = new Array(this.amountOfPlayers);
         let playersArr = this.players.fill(0);
         this.pushToCookie("players", playersArr);
         document.cookie = "currentplayer=0";
         this.generateWord();
-        console.log(document.cookie);
         this.init();
     }
 
@@ -288,7 +299,7 @@ class Game {
 
     // Cookie functions
 
-    createCookieKey(cookie){
+    createCookieKey(cookie) {
         document.cookie = cookie + "; expires=Tue, 19 Jan 2038 03:14:07 GMT"
     }
 
@@ -296,7 +307,7 @@ class Game {
         const stringState = JSON.stringify(data);
         const encodedArray = encodeURIComponent(stringState);
         document.cookie = key + "=" + encodedArray;
-    }        
+    }
 
     getDataFromCookie(key) {
         let cookieValue;
@@ -314,9 +325,9 @@ class Game {
         const currentData = this.getDataFromCookie(key);
         const decodedData = decodeURIComponent(currentData);
         let arrData;
-        if (key == "word"){
+        if (key == "word") {
             arrData = decodedData.split("");
-        } else if (key == "currentplayer"){
+        } else if (key == "currentplayer") {
             arrData = +(decodedData);
         } else {
             arrData = JSON.parse(decodedData);
@@ -332,7 +343,7 @@ class Game {
 const game1 = {
     root: ".game1",
     word: "мама",
-    players: 3,
+    players: 2,
     words: ["", "солома", "река", "крабик", "дом", "дверь"],
     name: "Mikhail"
 }
